ec9849bd542d5c19ef985bea3c6c58052bfd6fbf
Andrea Boa <andreagboa@gmail.com>
2023-03-31 02:35:48 +0200

    Main model in 2D

--- train.py
+++ train.py
@@ -38,7 +38,7 @@ def main():
     #To work with WandB
     if config['wandb']:
         run = wandb.init(
-            project="wgan-gp_bnd1", 
+            project="wgan-gp_scalar", 
             entity="andreathesis",
             config=config
         )
